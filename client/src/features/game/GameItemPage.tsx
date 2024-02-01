88d44fa53688a961c9c7a03e94fe761bec33f7d6
Ivan Kabatskii <ivankabatsky@gmail.com>
2024-02-01 16:22:54 +0300

    Main page component

---
+++ client/src/features/game/GameItemPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GameItemPage = () => {
+  return (
+    <div>GameItemPage</div>
+    // Здесь выводятся темы с вопросами
+  );
+};
+
+export default GameItemPage;
---
+++ client/src/features/game/GameListPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GameListPage = (): JSX.Element => {
+  return <div>GameListPage</div>;
+  //тут мапается список игр, пользователь выбирает одну и проваливается
+};
+
+export default GameListPage;
